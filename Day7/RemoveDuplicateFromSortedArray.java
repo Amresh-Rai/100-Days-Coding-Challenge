32f11eca9e5f85c662a10780825f11a3bc95e0da
Amresh Rai <amreshrai46200@gmail.com>
2025-09-05 21:22:16 +0530

    upload

---
+++ Day7/RemoveDuplicateFromSortedArray.java
@@ -0,0 +1,27 @@
+package Day7;
+
+public class RemoveDuplicateFromSortedArray {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int j = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[j]) {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,2,3,3,4,5,5};
+        int k = removeDuplicates(arr);
+
+        System.out.println("Unique count: " + k);
+        System.out.print("After removing duplicates: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
