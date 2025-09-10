1fe7216842dbe37ce8b57a9a39aa5ae4b383ea6d
Amresh Rai <amreshrai46200@gmail.com>
2025-09-10 23:22:57 +0530

    upload

--- Day2/MajorityElement.java
+++ Day2/MajorityElement.java
@@ -1,5 +1,21 @@
 package Day2;
 
 public class MajorityElement {
-    
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(arr)); // Output: 2
+    }
 }
