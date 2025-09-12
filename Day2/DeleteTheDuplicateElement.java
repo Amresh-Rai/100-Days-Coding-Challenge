e2bfead5af106d289cfe9f6c5f0d82de343ccfce
Amresh Rai <amreshrai46200@gmail.com>
2025-09-12 21:56:57 +0530

    upload

--- Day2/DeleteTheDuplicateElement.java
+++ Day2/DeleteTheDuplicateElement.java
@@ -1,5 +1,23 @@
 package Day2;
 
 public class DeleteTheDuplicateElement {
-    
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int j = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[j]) {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 3, 3, 4};
+        int len = removeDuplicates(arr);
+        for (int i = 0; i < len; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
