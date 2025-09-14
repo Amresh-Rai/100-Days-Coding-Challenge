d80fba0fc189b8bc0dd526874f3163630d23de3c
Amresh Rai <amreshrai46200@gmail.com>
2025-09-14 21:55:16 +0530

    upload

--- Day4/FindPairOfElement.java
+++ Day4/FindPairOfElement.java
@@ -2,8 +2,15 @@ package Day4;
 
 public class FindPairOfElement {
     public static void main(String[] args) {
-        int []arr={1,2,3,4,5,6};
-        
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int target = 7;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    System.out.println(arr[i] + ", " + arr[j]);
+                }
+            }
+        }
     }
-    
 }
